70bd2ff8191fdfcf4bc35538fc5ec09e6887071e
Nimira43 <n.rabone72@aol.com>
2024-12-08 13:42:12 +0000

    process data - calculate the mean of x and y values

--- python/main.py
+++ python/main.py
@@ -7,3 +7,6 @@ with open('../js/data.json', 'r') as f:
 
 x_values = [point['x'] for point in data]
 y_values = [point['y'] for point in data]
+
+mean_x = sum(x_values) / len(x_values)
+mean_y = sum(y_values) / len(y_values)
