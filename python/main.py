6d5dbdec7db8d4ca4e52377daf962d22744db543
Nimira43 <n.rabone72@aol.com>
2024-12-08 13:43:19 +0000

    Print out the mean values of x and y

--- python/main.py
+++ python/main.py
@@ -10,3 +10,6 @@ y_values = [point['y'] for point in data]
 
 mean_x = sum(x_values) / len(x_values)
 mean_y = sum(y_values) / len(y_values)
+
+print(f'Mean of X values: {mean_x}')
+print(f'Mean of Y values: {mean_y}')
